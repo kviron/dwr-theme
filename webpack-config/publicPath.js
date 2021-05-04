bf7b2784822846b59571b4af5fe51b75fbdbeed7
Roman Mamaev <69055185+kviron@users.noreply.github.com>
2021-05-04 12:19:30 +0300

    Update publicPath.js

--- webpack-config/publicPath.js
+++ webpack-config/publicPath.js
@@ -1,5 +1,5 @@
 const path = require('path');
 module.exports = (folder, prefix = '') => {
   const theme = path.basename(path.resolve('../'));
-  return `${prefix}/${folder}/`;
+  return `${ prefix }/app/themes/${ process.env.THEME_NAME }/assets/`;
 }
