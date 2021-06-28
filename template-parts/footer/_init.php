1fc0b3b677390edf2085577e980d1cdaec184a25
Roman Mamaev <69055185+kviron@users.noreply.github.com>
2021-06-28 10:34:50 +0300

    Update _init.php

--- template-parts/footer/_init.php
+++ template-parts/footer/_init.php
@@ -1,11 +1,14 @@
 <?php
+use DWR\DWR;
 
 add_action('site_footer', 'get_footer_site');
-function get_footer_site(){
-    DWR::get_template('template-parts/footer/footer');
+function get_footer_site()
+{
+    DWR::theTemplate('template-parts/footer/footer');
 }
 
 add_action('site_footer', 'get_footer_scripts_site');
-function get_footer_scripts_site(){
-    DWR::get_template('template-parts/footer/footer-scripts');
+function get_footer_scripts_site()
+{
+    DWR::theTemplate('template-parts/footer/footer-scripts');
 }
