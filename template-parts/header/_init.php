1a8fa78d4ec72a75b195e38304886c4eb45826cd
Roman Mamaev <69055185+kviron@users.noreply.github.com>
2021-09-23 15:02:51 +0300

    Update _init.php

--- template-parts/header/_init.php
+++ template-parts/header/_init.php
@@ -1,16 +1,16 @@
 <?php
-use DWR\DWR;
+use CE\CE;
 
 add_action('wp_head', 'get_meta_header_site');
 function get_meta_header_site()
 {
-    DWR::theTemplate('template-parts/header/meta-section');
+    CE::theTemplate('template-parts/header/meta-section');
 }
 
 add_action('site_header', 'get_header_site');
 function get_header_site()
 {
-    DWR::theTemplate('template-parts/header/header', [
-        'className' => DWR::getTypePage(),
+    CE::theTemplate('template-parts/header/header', [
+        'className' => CE::getPageType(),
     ]);
 }
